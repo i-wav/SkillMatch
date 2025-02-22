bdf21649906db6f72b494305bcc444ec33dd232e
s-sakshi9317 <io.sakshis9317@gmail.com>
2025-02-22 17:27:20 +0530

    Updated app.py to restore joblib import and ensure functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-# import joblib
+import joblib
+
 from sklearn.metrics.pairwise import cosine_similarity
 
 df_tech2 = pd.read_csv("./data/df_tech2.csv")
